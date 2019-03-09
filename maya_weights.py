d005db2af073e185c3c0412e559b53b1dae56a63
OleksiiZaitsev <oleksii.animation@gmail.com>
2019-03-09 11:33:06 +0300

    clear

--- maya_weights.py
+++ maya_weights.py
@@ -2,22 +2,19 @@ import time
 
 
 def mapping():
-	with open(fullpath, 'r') as f:
-		weights,Z = eval(f.read())
-		cmds.setAttr('blendShape3.x2_r_JDxLS', weights[0]*2)
-		
-		
-		cmds.setAttr('blendShape3.c_FN', weights[1])
-		cmds.setAttr('blendShape3.psd_surprised', weights[1]/2)
-		
-		cmds.setAttr('blendShape3.c_JD', weights[2])
-		cmds.setAttr('blendShape3.c_CRUL', weights[2])
-		Z = -int(Z)
-		#cmds.xform('Head', rotation = (0,0, Z))
-		
-	#time.sleep(1/30)	
-	cmds.refresh()
-		
+    with open(fullpath, 'r') as f:
+        weights,Z = eval(f.read())
+        cmds.setAttr('blendShape3.name', weights[0]*2)
+        cmds.setAttr('blendShape3.name', weights[1])
+        cmds.setAttr('blendShape3.name', weights[1]/2)
+        cmds.setAttr('blendShape3.name', weights[2])
+        cmds.setAttr('blendShape3.name', weights[2])
+        Z = -int(Z)
+        #cmds.xform('Head', rotation = (0,0, Z))
+        
+    #time.sleep(1/30)
+    cmds.refresh()
+        
 
 LABELS_LIST = ['1 - idle', '2 - jaw_open','3 - smile',]
 
